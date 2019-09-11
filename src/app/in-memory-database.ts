ab96d65737c0f673f34f39f105a4afd252fb4601
AnthonyAlmeida-exe <Anthonyl.almeida@gmail.com>
2019-09-11 14:54:57 -0300

    adding Category Module

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,17 +1,16 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Category } from "./pages/categories/shared/category.model";
 
-export class InMemoryDatabase implements InMemoryDbService{
-  
-  createDb(){
-    const categories = [
-      {id: 1, name:"lazer", description:"Aprendendo Api"},
-      {id: 2, name:"beber", description:"Aprendendo Api"},
-      {id: 3, name:"jogar bola", description:"Aprendendo Api"},
-      {id: 4, name:"feedar", description:"Aprendendo Api"},
-      {id: 5, name:"stompar", description:"Aprendendo Api"},
-      {id: 6, name:"kiting", description:"Aprendendo Api"},
-
+export class InMemoryDatabase implements InMemoryDbService {
+  createDb() {
+    const categories: Category[] = [
+      { id: 1, name: "lazer", description: "Aprendendo Api" },
+      { id: 2, name: "beber", description: "Aprendendo Api" },
+      { id: 3, name: "jogar bola", description: "Aprendendo Api" },
+      { id: 4, name: "feedar", description: "Aprendendo Api" },
+      { id: 5, name: "stompar", description: "Aprendendo Api" },
+      { id: 6, name: "kiting", description: "Aprendendo Api" }
     ];
-
+    return { categories };
   }
 }
